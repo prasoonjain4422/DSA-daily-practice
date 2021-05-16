479f6646c231b589e0c1a2403a702e5b33e943bf
Prasoon Jain <prasoonjain4422@gmail.com>
2021-05-16 14:27:49 +0530

    day16

--- BinaryTrees/a.cpp
+++ BinaryTrees/a.cpp
@@ -2,52 +2,122 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child */
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+Node *newNode(int val)
+{
+    Node *temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+    return temp;
+}
+Node *buildTree(string str)
+{
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node *root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node *> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size())
+    {
+
+        // Get and remove the front of the queue
+        Node *currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N")
+        {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N")
+        {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
 // } Driver Code Ends
+/* Tree node structure  used in the program
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+}; */
+
 class Solution
 {
 public:
-    string FirstNonRepeating(string A)
+    //Function to return the diameter of a Binary Tree.
+
+    Node* diameter(Node *root)
     {
 
-        string ans;
+        if (root == NULL)
+            return 0;
+        
+        
 
-        unordered_map<char, bool> um;
-        unordered_map<char, bool> um2;
-
-        int i = 0, j = 0;
-        for (i = 0; i < A.length(); i++)
-        {
-            if (!um[A[i]])
-            {
-                um[A[i]] = true;
-                um2[A[i]] = true;
-            }
-            else
-            {
-                um2[A[i]] = false;
-            }
-
-            while (j <= i)
-            {
-                if ((um[A[j]]) && (um2[A[j]]))
-                {
-                    ans = ans + A[j];
-                    break;
-                }
-                j++;
-            }
-
-            if (i < j)
-            {
-                ans.append("#");
-            }
-        }
-
-        return ans;
     }
 };
 
 // { Driver Code Starts.
+
+/* Driver program to test size function*/
 int main()
 {
 #ifndef ONLINE_JUDGE
@@ -57,15 +127,19 @@ int main()
 
     // ios_base::sync_with_stdio(false);
     // cin.tie(0);
-    int tc;
-    cin >> tc;
-    while (tc--)
+
+    int t;
+    scanf("%d\n", &t);
+    while (t--)
     {
-        string A;
-        cin >> A;
-        Solution obj;
-        string ans = obj.FirstNonRepeating(A);
-        cout << ans << "\n";
+        string s;
+        getline(cin, s);
+        Node *root = buildTree(s);
+        Solution ob;
+        root = ob.diameter(root);
+        
+        
     }
     return 0;
-} // } Driver Code Ends
+}
+// } Driver Code Ends
---
+++ BinaryTrees/inorder-iteration.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Data structure to store a binary tree node
+struct Node
+{
+    int data;
+    Node *left, *right;
+
+    Node(int data)
+    {
+        this->data = data;
+        this->left = this->right = nullptr;
+    }
+};
+
+// Recursive function to perform preorder traversal on the tree
+void inorder(Node *root)
+{
+
+    stack<Node *> s;
+    s.push(root);
+    Node *a = root->left;
+
+    while ((!s.empty()) || (a != NULL))
+    {
+        if (a == NULL)
+        {
+            a = s.top();
+            s.pop();
+            cout << a->data << " ";
+            a = a->right;
+        }
+        else
+        {
+            s.push(a);
+            a = a->left;
+        }
+    }
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    // ios_base::sync_with_stdio(false);
+    // cin.tie(0);
+
+    /* Construct the following tree
+               1
+             /   \
+            /     \
+           2       3
+          /      /   \
+         /      /     \
+        4      5       6
+              / \
+             /   \
+            7     8
+    */
+
+    Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->right->left = new Node(5);
+    root->right->right = new Node(6);
+    root->right->left->left = new Node(7);
+    root->right->left->right = new Node(8);
+
+    inorder(root);
+
+    return 0;
+}
