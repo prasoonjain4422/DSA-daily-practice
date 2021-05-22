4edfb95c0e689167dc5520e2fd78baa59d5f653e
Prasoon Jain <prasoonjain4422@gmail.com>
2021-05-23 02:30:51 +0530

    day21

--- BinarySearchTrees/kth-largest-element-in-BST.cpp
+++ BinarySearchTrees/kth-largest-element-in-BST.cpp
@@ -3,87 +3,83 @@
 using namespace std;
 
 // Tree Node
-struct Node
-{
+struct Node {
     int data;
     Node *left;
     Node *right;
 
-    Node(int val)
-    {
+    Node(int val) {
         data = val;
         left = right = NULL;
     }
 };
 
 // Function to Build Tree
-Node *buildTree(string str)
-{
+Node* buildTree(string str)
+{   
     // Corner Case
-    if (str.length() == 0 || str[0] == 'N')
-        return NULL;
-
-    // Creating vector of strings from input
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+    // Creating vector of strings from input 
     // string after spliting by space
     vector<string> ip;
-
+    
     istringstream iss(str);
-    for (string str; iss >> str;)
+    for(string str; iss >> str; )
         ip.push_back(str);
-
+        
     // Create the root of the tree
-    Node *root = new Node(stoi(ip[0]));
-
+    Node* root = new Node(stoi(ip[0]));
+        
     // Push the root to the queue
-    queue<Node *> queue;
+    queue<Node*> queue;
     queue.push(root);
-
+        
     // Starting from the second element
     int i = 1;
-    while (!queue.empty() && i < ip.size())
-    {
-
+    while(!queue.empty() && i < ip.size()) {
+            
         // Get and remove the front of the queue
-        Node *currNode = queue.front();
+        Node* currNode = queue.front();
         queue.pop();
-
+            
         // Get the current node's value from the string
         string currVal = ip[i];
-
+            
         // If the left child is not null
-        if (currVal != "N")
-        {
-
+        if(currVal != "N") {
+                
             // Create the left child for the current node
             currNode->left = new Node(stoi(currVal));
-
+                
             // Push it to the queue
             queue.push(currNode->left);
         }
-
+            
         // For the right child
         i++;
-        if (i >= ip.size())
+        if(i >= ip.size())
             break;
         currVal = ip[i];
-
+            
         // If the right child is not null
-        if (currVal != "N")
-        {
-
+        if(currVal != "N") {
+                
             // Create the right child for the current node
             currNode->right = new Node(stoi(currVal));
-
+                
             // Push it to the queue
             queue.push(currNode->right);
         }
         i++;
     }
-
+    
     return root;
 }
 
-// } Driver Code Ends
+
+ // } Driver Code Ends
 /*The Node structure is defined as
 struct Node {
     int data;
@@ -100,27 +96,28 @@ struct Node {
 // return the Kth largest element in the given BST rooted at 'root'
 class Solution
 {
-public:
+    public:
+    
     int fun(Node *root, int *k)
     {
-        if (root == NULL)
+        if(root == NULL)
             return -1;
-
+        
         int l, r;
-        l = fun(root->left, k);
-        if (l != -1)
-            return l;
-
+        r = fun(root->right, k);
+        if(r != -1)
+            return r;
+        
         (*k)--;
-        if (*k == 0)
+        if(*k==0)
         {
             return (root->data);
         }
-
-        r = fun(root->right, k);
-        return r;
+        
+        l = fun(root->left, k);
+        return l;
     }
-
+    
     int kthLargest(Node *root, int k)
     {
         return fun(root, &k);
@@ -131,29 +128,22 @@ public:
 
 int main()
 {
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
-
-    // ios_base::sync_with_stdio(false);
-    // cin.tie(0);
     int t;
-    cin >> t;
+    cin>>t;
     getchar();
-
-    while (t--)
+    
+    while(t--)
     {
         string s;
-        getline(cin, s);
-        Node *head = buildTree(s);
-
+        getline(cin,s);
+        Node* head = buildTree(s);
+        
         int k;
-        cin >> k;
+        cin>>k;
         getchar();
-
+        
         Solution ob;
-        cout << ob.kthLargest(head, k) << endl;
+        cout << ob.kthLargest( head, k ) << endl;
     }
-    return 0;
-} // } Driver Code Ends
+    return 1;
+}  // } Driver Code Ends
