08c5346836973a285695061a923be17fb26f1cc1
Prasoon Jain <prasoonjain4422@gmail.com>
2021-05-24 20:26:56 +0530

    day23

---
+++ SearchingAndSorting/find-pair-of-given-differences.cpp
@@ -0,0 +1,51 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool findPair(int arr[], int size, int n);
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int l, n;
+        cin >> l >> n;
+        int arr[l];
+        for (int i = 0; i < l; i++)
+            cin >> arr[i];
+        if (findPair(arr, l, n))
+            cout << 1 << endl;
+        else
+            cout << "-1" << endl;
+    }
+
+    return 0;
+} // } Driver Code Ends
+
+bool findPair(int a[], int size, int n)
+{
+    sort(a, a + size);
+    int i = 0, j = 1;
+    while ((i < j) && (i < size) && (j < size))
+    {
+        if (a[j] - a[i] == n)
+        {
+            return true;
+        }
+        else if (a[j] - a[i] < n)
+        {
+            j++;
+        }
+        else
+        {
+            i++;
+            if (i == j)
+                j++;
+        }
+    }
+
+    return false;
+}
