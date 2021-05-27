330cc3107ca8bd235f08673de8595ff910438e5c
Prasoon Jain <prasoonjain4422@gmail.com>
2021-05-27 15:20:25 +0530

    day25

--- Array/max-pro-subarray.cpp
+++ Array/max-pro-subarray.cpp
@@ -3,66 +3,76 @@
 
 using namespace std;
 
- // } Driver Code Ends
+// } Driver Code Ends
 //User function template for C++
-class Solution{
+class Solution
+{
 public:
-
-	// Function to find maximum product subarray
-	long long maxProduct(int *a, int n) {
-	    
-    long long int pro = 1, tln = 1, f = 1, ans = 1, i;
-    for (i = 0; i < n; i++)
+    // Function to find maximum product subarray
+    long long maxProduct(int *a, int n)
     {
-        if (a[i] > 0)
+
+        long long int pro = 1, tfn = 1, f = 1, ans = 1, i;
+        for (i = 0; i < n; i++)
         {
-            pro *= a[i];
-            if (f == 1)
-                tln *= a[i];
-        }
-        else if (a[i] < 0)
-        {
-            ans = max(ans, pro);
-            pro *= a[i];
-            if (f == 1)
+            if (a[i] > 0)
             {
-                tln *= a[i];
-                f = 0;
+                pro *= a[i];
+                if (f == 1)
+                    tfn *= a[i];
+            }
+            else if (a[i] < 0)
+            {
+                ans = max(ans, pro);
+                pro *= a[i];
+                if (f == 1)
+                {
+                    tfn *= a[i];
+                    f = 0;
+                }
+            }
+            else
+            {
+                if (pro < 0)
+                    ans = max(ans, pro / tfn);
+                else
+                    ans = max(ans, pro);
+
+                pro = 1;
+                tfn = 1;
+                f = 1;
             }
         }
+        if (pro < 0)
+            ans = max(ans, pro / tfn);
         else
-        {
-            if (pro < 0)
-                ans = max(ans, pro / tln);
-            else
-                ans = max(ans, pro);
+            ans = max(ans, pro);
 
-            pro = 1;
-            tln = 1;
-            f = 1;
-        }
+        return ans;
     }
-    if (pro < 0)
-        ans = max(ans, pro / tln);
-    else
-        ans = max(ans, pro);
-    
-    
-	return ans;
-	
-	}
 };
 
 // { Driver Code Starts.
 
-int main() {
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    // ios_base::sync_with_stdio(false);
+    // cin.tie(0);
     int t;
     cin >> t;
-    while (t--) {
+    while (t--)
+    {
         int n, i;
         cin >> n;
         int arr[n];
-        for (i = 0; i < n; i++) {
+        for (i = 0; i < n; i++)
+        {
             cin >> arr[i];
         }
         Solution ob;
@@ -70,4 +80,4 @@ int main() {
         cout << ans << "\n";
     }
     return 0;
-}  // } Driver Code Ends
+} // } Driver Code Ends
