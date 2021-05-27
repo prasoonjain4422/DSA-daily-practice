e9c4a19230f98029d5a23be49bcbe6bae5813706
Prasoon Jain <prasoonjain4422@gmail.com>
2021-05-27 13:03:32 +0530

    day24

--- SearchingAndSorting/allocate-minimum-number-of-pages.cpp
+++ SearchingAndSorting/allocate-minimum-number-of-pages.cpp
@@ -1,22 +1,21 @@
 // { Driver Code Starts
 // Initial template for C++
 
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-// } Driver Code Ends
+
+ // } Driver Code Ends
 //User function template in C++
 
-class Solution
+class Solution 
 {
-public:
+    public:
     //Function to find minimum number of pages.
-    int findPages(int a[], int n, int m)
+        int findPages(int a[], int n, int m)
     {
 
-        // sort(a, a + n);
-
-        int i, j, l = 1, r=0, mid, s, ans = INT_MAX;
+        int i, j, l = 0, r = 0, mid, s, ans = INT_MAX;
         for (i = 0; i < n; i++)
             r += a[i];
 
@@ -28,18 +27,24 @@ public:
             i = 0;
             s = 0;
             j = m;
-            ans = 0;
-            cout << mid << " mid " << endl;
+
+            // cout << mid << " mid " << endl;
             while (i < n)
             {
-                if (s >= mid)
+                
+                if(a[i] > mid)
+                {
+                    break;
+                }
+                
+
+                if ((s + a[i]) > mid)
                 {
                     j--;
-                    s = 0;
-                    if (j <= 0)
+                    if (j == 0)
                         break;
-
-                    i--;
+                    // cout << "j-- " << i << endl;
+                    s = a[i];
                 }
                 else
                 {
@@ -48,49 +53,42 @@ public:
 
                 i++;
             }
-            if (j <= 0)
+            if (i >= n)
             {
-                cout << "in l" << endl;
-                ans = mid;
-                l = mid + 1;
+                // cout << "in r" << endl;
+                ans = min(ans, mid);
+                r = mid - 1;
             }
             else
             {
-                cout << "in r" << endl;
-                r = mid - 1;
+                // cout << "in l" << endl;
+                l = mid + 1;
             }
 
-            cout << ans << endl;
+            // cout << ans << endl;
         }
 
-        return min(ans, mid);
+        return ans;
     }
 };
 
-int main()
-{
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
+// { Driver Code Starts.
 
-    // ios_base::sync_with_stdio(false);
-    // cin.tie(0);
+int main() {
     int t;
-    cin >> t;
-    while (t--)
-    {
+    cin>>t;
+    while(t--){
         int n;
-        cin >> n;
+        cin>>n;
         int A[n];
-        for (int i = 0; i < n; i++)
-        {
-            cin >> A[i];
+        for(int i=0;i<n;i++){
+            cin>>A[i];
         }
         int m;
-        cin >> m;
+        cin>>m;
         Solution ob;
         cout << ob.findPages(A, n, m) << endl;
     }
     return 0;
 }
+  // } Driver Code Ends
