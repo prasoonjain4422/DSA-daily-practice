11148f0b6c0ce72334dd74f390995f1d56b646ed
Prasoon Jain <prasoonjain4422@gmail.com>
2021-06-06 20:39:02 +0530

    day35

---
+++ Heap/min-cost-of-ropes.cpp
@@ -0,0 +1,57 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+
+class Solution
+{
+    public:
+    //Function to return the minimum cost of connecting the ropes.
+    long long minCost(long long arr[], long long n) 
+    {
+        
+        priority_queue<int> pq;
+        long long i, s = 0;
+        
+        for(i=0; i<n; i++)
+        {
+            pq.push(a[i]);
+        }
+        
+        s = pq.top();
+        pq.pop();
+        while(!pq.empty())
+        {
+            i = pq.top();
+            pq.pop();
+            
+            s += (s + i);
+            
+        }
+        
+        
+        return s;
+    }
+};
+
+
+// { Driver Code Starts.
+long long minCost(long long arr[], long long n);
+int main() {
+    long long t;
+    cin >> t;
+    while (t--) {
+        long long n;
+        cin >> n;
+        long long i, a[n];
+        for (i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        Solution ob;
+        cout << ob.minCost(a, n) << endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
