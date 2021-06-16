0ea8db86eb82a1338ea2119f435afd9be04f8c7e
Prasoon Jain <prasoonjain4422@gmail.com>
2021-06-16 07:46:01 +0530

    day45

--- DynamicProgramming/longest-palindromic-substring.cpp
+++ DynamicProgramming/longest-palindromic-substring.cpp
@@ -1,9 +1,5 @@
-// { Driver Code Starts
-// Counts Palindromic Subsequence in a given String
 #include <bits/stdc++.h>
 using namespace std;
-
-// } Driver Code Ends
 
 string a;
 const int M = 1000000007;
@@ -11,38 +7,14 @@ long long int dp[1000][1000];
 class Solution
 {
 public:
-    /*You are required to complete below method */
-    void countPS(string &s, int i, int j)
+    string lpsub(string &s, int i, int j)
     {
-
-        if (i > j)
-            return;
-
-        if (s[i] == s[j])
-        {
-            a = a + s[i];
-            countPS(s, i + 1, j - 1);
-            a = a + s[j];
-        }
-        else
-        {
-            a = "";
-            countPS(s, i + 1, j);
-            string b = a;
-            a = "";
-            countPS(s, i, j - 1);
-            if (b.length() > a.length())
-            {
-                a = b;
-            }
-        }
         
-        return;
     }
 };
+        
+        
 
-// { Driver Code Starts.
-// Driver program
 int main()
 {
 #ifndef ONLINE_JUDGE
@@ -59,7 +31,6 @@ int main()
         a = "";
         memset(dp, -1, sizeof(dp));
         Solution ob;
-        ob.countPS(str, 0, str.length() - 1);
-        cout << a << endl;
+        cout << ob.lpsub(str) << endl;
     }
-} // } Driver Code Ends
+}
