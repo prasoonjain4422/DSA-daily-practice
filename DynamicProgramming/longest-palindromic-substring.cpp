d619c1e6e2a619e79e9e799eeeb0e811d872b197
Prasoon Jain <prasoonjain4422@gmail.com>
2021-06-18 23:19:13 +0530

    day45

--- DynamicProgramming/longest-palindromic-substring.cpp
+++ DynamicProgramming/longest-palindromic-substring.cpp
@@ -7,13 +7,54 @@ long long int dp[1000][1000];
 class Solution
 {
 public:
-    string lpsub(string &s, int i, int j)
+    string lpsub(string s)
     {
-        
+        int i, j;
+        int n = s.length();
+        bool dp[n + 1][n + 1];
+        string ans = "";
+
+        for (i = 0; i <= n; i++)
+        {
+            for (j = 0; j <= n; j++)
+            {
+                if ((i == 0) || (j == 0))
+                {
+                    dp[i][j] = true;
+                }
+                else if (i == j)
+                {
+                    dp[i][j] = true;
+                }
+                else if (s[i - 1] == s[j - 1])
+                {
+                    if (j - 1 > i)
+                    {
+                        dp[i][j] = ((dp[i + 1][j - 1]));
+                    }
+                    else
+                    {
+                        dp[i][j] = true;
+                    }
+
+                    if (dp[i][j] == true)
+                    {
+                        if ((j - i + 1) > ans.length())
+                        {
+                            ans = s.substr(i - 1, j - i - 1);
+                        }
+                    }
+                }
+                else
+                {
+                    dp[i][j] = false;
+                }
+            }
+        }
+
+        return ans;
     }
 };
-        
-        
 
 int main()
 {
