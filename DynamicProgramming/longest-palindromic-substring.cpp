d507d2d5cea9fb548fb808d5e2119cb2ef35fc3c
Prasoon Jain <prasoonjain4422@gmail.com>
2021-06-19 10:56:43 +0530

    day45

--- DynamicProgramming/longest-palindromic-substring.cpp
+++ DynamicProgramming/longest-palindromic-substring.cpp
@@ -1,7 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string a;
 const int M = 1000000007;
 long long int dp[1000][1000];
 class Solution
@@ -9,50 +8,50 @@ class Solution
 public:
     string lpsub(string s)
     {
-        int i, j;
+        int i, j, k, start = 0, len = 1;
         int n = s.length();
         bool dp[n + 1][n + 1];
-        string ans = "";
+        memset(dp, false, sizeof(dp));
+        
 
-        for (i = 0; i <= n; i++)
+        for (i = 1; i <= n; i++)
         {
-            for (j = 0; j <= n; j++)
+            dp[i][i] = true;
+        }
+
+        for (i = 1; i <= n; i++)
+        {
+            if (s[i - 1] == s[i])
             {
-                if ((i == 0) || (j == 0))
+                dp[i][i + 1] = true;
+
+                len = 2;
+                start = i - 1;
+            }
+        }
+
+        for (k = 3; k <= n; k++)
+        {
+            for (i = 1; i <= n - k + 1; i++)
+            {
+                j = i + k - 1;
+                if (j <= n)
                 {
-                    dp[i][j] = true;
-                }
-                else if (i == j)
-                {
-                    dp[i][j] = true;
-                }
-                else if (s[i - 1] == s[j - 1])
-                {
-                    if (j - 1 > i)
-                    {
-                        dp[i][j] = ((dp[i + 1][j - 1]));
-                    }
-                    else
+                    if ((dp[i + 1][j - 1]) && (s[i - 1] == s[j - 1]))
                     {
                         dp[i][j] = true;
-                    }
 
-                    if (dp[i][j] == true)
-                    {
-                        if ((j - i + 1) > ans.length())
+                        if (len < k)
                         {
-                            ans = s.substr(i - 1, j - i - 1);
+                            len = k;
+                            start = i - 1;
                         }
                     }
-                }
-                else
-                {
-                    dp[i][j] = false;
                 }
             }
         }
 
-        return ans;
+        return (s.substr(start, len));
     }
 };
 
@@ -69,8 +68,6 @@ int main()
     {
         string str;
         cin >> str;
-        a = "";
-        memset(dp, -1, sizeof(dp));
         Solution ob;
         cout << ob.lpsub(str) << endl;
     }
