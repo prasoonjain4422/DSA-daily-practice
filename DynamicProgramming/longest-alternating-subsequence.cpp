42b099598e6f89b66732e2a037215faf3d90865b
Prasoon Jain <prasoonjain4422@gmail.com>
2021-06-21 18:21:50 +0530

    day47

--- DynamicProgramming/longest-alternating-subsequence.cpp
+++ DynamicProgramming/longest-alternating-subsequence.cpp
@@ -1,77 +1,48 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+	public:
     int AlternatingaMaxLength(vector<int> &a)
     {
 
-        int i, j, k, len = 2, n = a.size();
-
-        if ((n == 1) || (n == 2))
+        int inc = 1;
+        int dec = 1;
+        int i;
+        
+        
+        for(i=1; i<a.size(); i++)
         {
-            return n;
-        }
-
-        int dp[n];
-        int ad[n];
-        memset(dp, 0, sizeof(dp));
-        memset(ad, 0, sizeof(ad));
-        dp[0] = 1;
-        dp[1] = 2;
-        if (a[0] > a[1])
-        {
-            ad[1] = 1;
-        }
-        else
-        {
-            ad[1] = -1;
-        }
-
-
-        for (i = 2; i < n; i++)
-        {
-            dp[i] = 1;
-            ad[i] = 0;
-            for (j = i - 1; j >= 0; j--)
+            if(a[i-1] < a[i])
             {
-
-                if (ad[j] == 0)
-                {
-                    
-                    if(dp[i] == 1)
-                    {
-                        
-                        dp[i] = 1;
-                        if (a[j] > a[i])
-                        {
-                            ad[i] = 1;
-                        }
-                        else
-                        {
-                            ad[i] = -1;
-                        }
-                        
-                    }
-                }
-                else if ((ad[j] == 1) && (a[j] < a[i]))
-                {
-                    if (dp[i] < dp[j])
-                    {
-                        dp[i] = dp[j];
-                        ad[i] = -1;
-                    }
-                }
-                else if ((ad[j] == -1) && (a[j] > a[i]))
-                {
-                    if (dp[i] < dp[j])
-                    {
-                        dp[i] = dp[j];
-                        ad[i] = 1;
-                    }
-                }
+                inc = dec + 1;
             }
-            dp[i]++;
-            cout << i << " " << dp[i] << endl;
-
-            if (len < dp[i])
-                len = dp[i];
+            else if(a[i-1] > a[i])
+            {
+                dec = inc + 1;
+            }
         }
+        
+        
+        return max(inc, dec);
 
-        return len;
     }
+};
+
+// { Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		vector<int>nums(n);
+		for(int i = 0; i < n; i++)
+			cin >> nums[i];
+		Solution obj;
+		int ans = obj.AlternatingaMaxLength(nums);
+		cout << ans << "\n";
+	}
+	return 0;
+}  // } Driver Code Ends
