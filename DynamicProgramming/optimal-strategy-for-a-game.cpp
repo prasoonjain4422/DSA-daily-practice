387bc333dc6324f6e5a718a1cb2a3d5fadf05fbe
Prasoon Jain <prasoonjain4422@gmail.com>
2021-07-06 19:56:51 +0530

    day 50

---
+++ DynamicProgramming/optimal-strategy-for-a-game.cpp
@@ -0,0 +1,44 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+long long maximumAmount(int a[], int n)
+{
+    
+    
+    
+    
+    
+    
+}
+
+
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    //taking total testcases
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        //taking number of elements
+        int N;
+        cin >> N;
+
+        int A[N];
+
+        //inserting the elements
+        for (int i = 0; i < N; i++)
+            cin >> A[i];
+
+        //calling function maximumAmount()
+        cout << maximumAmount(A, N) << endl;
+    }
+    return 0;
+} // } Driver Code Ends
