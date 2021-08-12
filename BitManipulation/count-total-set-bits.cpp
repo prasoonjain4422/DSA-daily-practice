6f09e83a6a5249c128b1e0a89346b64da7a4e9bf
Prasoon Jain <prasoonjain4422@gmail.com>
2021-08-13 02:50:21 +0530

    57

---
+++ BitManipulation/count-total-set-bits.cpp
@@ -0,0 +1,46 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    // n: input to count the number of set bits
+    //Function to return sum of count of set bits in the integers from 1 to n.
+    int countSetBits(int n)
+    {
+        
+        if(n == 0)
+            return n;
+        
+        int i = log2(n);
+        
+        int ans = i*pow(2, i-1) + (n - pow(2, i) + 1) + countSetBits(n-pow(2, i));
+        
+        return ans;
+    }
+};
+
+
+// { Driver Code Starts.
+
+// Driver code
+int main()
+{
+	 int t;
+	 cin>>t;// input testcases
+	 while(t--) //while testcases exist
+	 {
+	       int n;
+	       cin>>n; //input n
+	       Solution ob;
+	       cout << ob.countSetBits(n) << endl;// print the answer
+	  }
+	  return 0;
+}
+  // } Driver Code Ends
