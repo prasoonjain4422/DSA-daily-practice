9c72cb431eb6f2147f27357875a929689f5c9e91
Prasoon Jain <prasoonjain4422@gmail.com>
2021-08-13 16:29:36 +0530

    57

---
+++ BitManipulation/divide-2-numbers-with-bits.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int i, j, a, b, ans = 0, temp = 0;
+    cin >> a >> b; //  (a/b)
+            cout<<i<<" rthgf ";
+
+    for (i = 31; i >= 0; i--)
+    {
+        if ((temp + (b << i)) <= a)
+        {
+            temp += (b << i);
+            ans = ans | (1LL << i);
+            cout<<i<<" rthgf ";
+            cout<<(temp + (b << i))<<endl;
+        }
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
