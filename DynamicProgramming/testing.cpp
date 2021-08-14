71971762c063fbeb0a0a3ca0a49212ff384785d5
Prasoon Jain <prasoonjain4422@gmail.com>
2021-08-15 01:31:13 +0530

    day59

--- DynamicProgramming/testing.cpp
+++ DynamicProgramming/testing.cpp
@@ -1,27 +1,56 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bool cmp(int a[2], int b[2])
+{
+    return (a[0] < b[0]);
+}
+
 int main()
 {
 #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 #endif
-    int T;
-    cin >> T;
-    while (T--)
+
+    int i, j, k, x, y, n, m, len, ans = 0;
+    cin >> n >> m;
+
+    vector<vector<int>> a(n, vector<int>(m));
+
+    for (i = 0; i < n; i++)
     {
-        int n = 18;
-        int i = log2(n);
-        
-        
-        while(n>0)
+        for (j = 0; j < m; j++)
         {
-            cout<<n<<endl;
-            i = log2(n);
-            n = n&(~(1<<i));
+            cin >> a[i][j];
         }
-        
     }
+
+
+    for (len = 1; len <= n; len++)
+    {
+        for (i = 0; (i+len) <= n; i++)
+        {
+            j = i + len - 1;
+            
+            for(k=0; k<m; k++)
+            {
+                y = 0;
+                for(x=i; x<=j; x++)
+                {
+                    y += a[x][k];
+                }
+                
+                if(y%2 != 0)
+                    break;
+            }
+            
+            if(k==m)
+                ans++;
+        }
+    }
+
+    cout << ans;
+
     return 0;
 }
