6d391121f5fa8ac0cb38b493ee69578a18e15106
Prasoon Jain <prasoonjain4422@gmail.com>
2021-09-08 15:17:40 +0530

    day 70

--- DynamicProgramming/a.cpp
+++ DynamicProgramming/a.cpp
@@ -9,38 +9,105 @@ int main()
     freopen("output.txt", "w", stdout);
 #endif
 
-    int i, j, n, k;
+    int i, j, n, k, d = 0;
 
-    cin >> k;
-    n = 1;
-    i = 10;
+    cin >> n >> k;
+    // cout << n << k << endl;
 
+    string s = "";
 
-    if (k % 2 == 0)
+    while (n > 0)
     {
-        cout << -1 << endl;
-        return 0;
+        s = to_string((n % 10)) + s;
+        n = n / 10;
+        d++;
     }
 
-    if (k % 5 == 0)
+    for (i = 0; i < d; i++)
     {
-        cout << -1 << endl;
-        return 0;
-    }
-
-    while (true)
-    {
-        if (n % k == 0)
+        if (s[i] != s[d - 1 - i])
         {
-            cout << n << endl;
             break;
         }
-        else
-        {
-            n += i;
-            i *= 10;
-        }
     }
+
+    if (i >= d)
+    {
+        k++;
+        // cout << "inc" << endl;
+    }
+
+    int c = 0, p = 0;
+    while (k > c)
+    {
+
+        // cout << "inc" << endl;
+        // c = 0;
+        p = 0;
+        i = (d - 1) / 2;
+        // cout << d;
+        if (d % 2 == 0)
+            j = i + 1;
+        else
+            j = i;
+
+        while (i >= 0)
+        {
+
+            if (s[i] < s[j])
+                c += 10 - ((int)(s[i]) + 1 - 48) + 10 * p;
+            else
+                c += 10 - ((int)(s[j]) - 48) + 10 * p;
+
+            // cout << c << endl;
+            if (c >= k)
+            {
+                s[i] = ((char)(9 - c + k + 48));
+                s[j] = s[i];
+                break;
+            }
+            else
+            {
+                i--;
+                j++;
+                p++;
+            }
+        }
+
+        if (c >= k)
+        {
+            break;
+        }
+
+        // cout << s << endl;
+        for (int x = 0; x < d; x++)
+        {
+            s[x] = 48;
+        }
+        s = "1" + s;
+        d++;
+        // cout << s << endl;
+    }
+
+    while (i >= 0)
+    {
+        if (s[i] < s[j])
+            s[i] = (char)((int)s[i] + 1);
+        else
+            s[i] = s[j];
+
+        s[j] = s[i];
+        i--;
+        j++;
+    }
+
+    if (s[0] == '0')
+    {
+        s[0] = '1';
+        s[d - 1] = '1';
+    }
+
+    cout << s;
 
     return 0;
 }
--- oops/special-class.cpp
+++ oops/special-class.cpp
@@ -1,12 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// How will you make a class in such a way that object
+//  of that class can only be made from new operator
+// and if tried directly it will show compile error ?
+
+
+// answer = By making destructor private.
 
 
 
-
-
-
+class Cap
+{
+    private:
+        int a;
+        ~Cap()
+        {}
+};
 
 int main()
 {
@@ -15,5 +25,15 @@ int main()
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 #endif
+    
+    Cap *c = new Cap();
+    
+    
+    
+    
+    
+    
+    
+    
     return 0;
 }
