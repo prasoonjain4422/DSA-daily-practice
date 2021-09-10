e0724e2cbebd5ba5153a97237147be91d5bb3384
Prasoon Jain <prasoonjain4422@gmail.com>
2021-09-10 23:43:58 +0530

    day 73

---
+++ DynamicProgramming/a.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int i, j, n, k, d = 0;
+
+    cin >> n >> k;
+    
+    
+    
+
+    
+    
+       
+        
+    
+    
+    
+    
+    return 0;
+}
