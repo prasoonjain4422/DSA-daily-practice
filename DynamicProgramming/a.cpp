9910b7769a6026d74535e7e44a30879d42640307
Prasoon Jain <prasoonjain4422@gmail.com>
2021-09-18 23:36:48 +0530

    day 79

--- DynamicProgramming/a.cpp
+++ DynamicProgramming/a.cpp
@@ -1,126 +1,148 @@
+// { Driver Code Starts
 #include <bits/stdc++.h>
 using namespace std;
-unordered_map<string, bool> vis;
+// UNASSIGNED is used for empty cells in sudoku grid
+#define UNASSIGNED 0
 
-bool isv(string str)
+// N is used for the size of Sudoku grid.
+#define N 9
+#define n 9
+// Size will be NxN
+
+// } Driver Code Ends
+
+unordered_map<string, bool> row, col;
+unordered_map<string, bool> sqr;
+class Solution
 {
-    int i, n = str.size();
-    stack<char> s;
-
-    for (i = 0; i < n; i++)
+public:
+    static bool dfs(int i, int j, int grid[N][N])
     {
-        if (str[i] == '(')
+
+        if ((i >= n - 1) && (j >= n))
+            return true;
+
+        while (i < n)
         {
-            s.push('(');
-        }
-        else if (str[i] == ')')
-        {
-            if ((!s.empty()) && (s.top() == '('))
+            // cout << " gggg " << i << " " << j << endl;
+            while (j < n)
             {
-                s.pop();
+                int k = 1;
+                if (grid[i][j] == 0)
+                {
+                    while (k < 10)
+                    {
+                        // cout << " gggg " << i << " " << j << " " << k << endl;
+                        // if (row[to_string(i) + to_string(k)])
+                        //     cout << "r" << endl;
+
+                        if ((!row[to_string(i) + to_string(k)]) && (!col[to_string(j) + to_string(k)]) && (!sqr[to_string(i / 3) + to_string(j / 3) + to_string(k)]))
+                        {
+                            // cout << " in" << endl;
+                            row[to_string(i) + to_string(k)] = true;
+                            col[to_string(j) + to_string(k)] = true;
+                            sqr[to_string(i / 3) + to_string(j / 3) + to_string(k)] = true;
+
+                            grid[i][j] = k;
+
+                            if (dfs(i, j + 1, grid))
+                            {
+                                cout << i << " " << j << " " << k << endl;
+                                return true;
+                            }
+
+                            grid[i][j] = 0;
+                            row[to_string(i) + to_string(k)] = false;
+                            col[to_string(j) + to_string(k)] = false;
+                            sqr[to_string(i / 3) + to_string(j / 3) + to_string(k)] = false;
+                        }
+                        k++;
+                    }
+
+                    cout << grid[i][j - 1] << " ";
+                    cout << i << " " << j << endl;
+                    return false;
+                }
+                j++;
             }
-            else
-            {
-                s.push(')');
-            }
+
+            i++;
+            j = 0;
         }
+
+        return false;
     }
 
-    if (s.empty())
-        return true;
-
-    return false;
-}
-
-void dfs(string str, int j, vector<string> &ans)
-{
-    
-    if(vis[str])
-        return;
-    
-    // cout << str << endl;
-    if (j == 0)
+    bool SolveSudoku(int grid[N][N])
     {
-        if (isv(str))
-        {
-            ans.push_back(str);
-        }
-    }
-    else
-    {
-        int i, n = str.size();
 
-        for (i = 0; i < n - 1; i++)
-        {
-            dfs(str.substr(0, i) + str.substr(i + 1), j - 1, ans);
-        }
-        dfs(str.substr(0, n - 1), j - 1, ans);
-    }
-    
-    vis[str] = true;
-    
-}
+        int i, j;
 
-vector<string> removeInvalidParentheses(string str)
-{
+        row.clear();
+        col.clear();
+        sqr.clear();
 
-    int i, j, n = str.size();
-    vector<int> l, r;
-    stack<char> s;
-
-    for (i = 0; i < n; i++)
-    {
-        if (str[i] == '(')
+        for (i = 0; i < n; i++)
         {
-            s.push('(');
-        }
-        else if (str[i] == ')')
-        {
-            if ((!s.empty()) && (s.top() == '('))
+            for (j = 0; j < n; j++)
             {
-                s.pop();
-            }
-            else
-            {
-                s.push(')');
+                if (grid[i][j] != 0)
+                {
+                    row[to_string(i) + to_string(grid[i][j])] = true;
+                    col[to_string(j) + to_string(grid[i][j])] = true;
+                    sqr[to_string(i / 3) + to_string(j / 3) + to_string(grid[i][j])] = true;
+                }
             }
         }
+
+        if (dfs(0, 0, grid))
+            return true;
+
+        return false;
     }
 
-    vector<string> ans;
-    j = s.size();
-    cout << j << endl;
-
-    for (i = 0; i < n - 1; i++)
+    //Function to print grids of the Sudoku.
+    void printGrid(int grid[N][N])
     {
-        dfs(str.substr(0, i) + str.substr(i + 1), j - 1, ans);
+        int i, j;
+        for (i = 0; i < n; i++)
+        {
+            for (j = 0; j < n; j++)
+            {
+                cout << grid[i][j] << " ";
+            }
+            cout << endl;
+        }
     }
+};
 
-    dfs(str.substr(0, n - 1), j - 1, ans);
-
-    return ans;
-}
+// { Driver Code Starts.
 
 int main()
 {
-
 #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 #endif
-
-    string s;
-    cin >> s;
-
-    vector<string> out = removeInvalidParentheses(s);
-
-    if (out.size() == 0)
-        cout << "0/-";
-
-    for (auto x : out)
+    int t;
+    cin >> t;
+    while (t--)
     {
-        cout << x << endl;
+        int grid[N][N];
+
+        for (int i = 0; i < 9; i++)
+            for (int j = 0; j < 9; j++)
+                cin >> grid[i][j];
+
+        Solution ob;
+
+        if (ob.SolveSudoku(grid) == true)
+            ob.printGrid(grid);
+        else
+            cout << "No solution exists";
+
+        cout << endl;
     }
 
     return 0;
-}
+} // } Driver Code Ends
--- DynamicProgramming/code_here.cpp
+++ DynamicProgramming/code_here.cpp
@@ -9,14 +9,49 @@ int main()
     freopen("output.txt", "w", stdout);
 #endif
 
-    int i, j, n, k, d = 0;
+    int i, j, n, k, d = 0, t;
 
-    cin >> n;
-    
-    
-    
+    cin >> t;
+    string s;
+    while (t--)
+    {
+        cin >> n;
+        vector<int> a(n), b(n);
+
+        for (i = 0; i < n; i++)
+            cin >> a[i];
+
+        unordered_map<int, int> um, vis;
+
+        for (i = 0; i < n; i++)
+        {
+            cin >> b[i];
+            b[i]--;
+            um[b[i]] = i;
+        }
+
+        int ans = 0, cyc = 0;
+
+        for (i = 0; i < n; i++)
+        {
+            
+            if((vis[i])||(um[a[i]] == j))
+                continue;
+            
+            j = i;
+            cyc = 0;
+            while (!vis[j])
+            {
+                vis[j] = true;
+                cyc++;
+                j = um[a[j]];
+            }
+
+            ans += max(0, cyc - 1);
+        }
+
+        cout << ans << endl;
+    }
+
     return 0;
 }
-
-
-
