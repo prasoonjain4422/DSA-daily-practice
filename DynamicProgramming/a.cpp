5091064d008d6cb55649e5425e98c50fdb910736
Prasoon Jain <prasoonjain4422@gmail.com>
2021-09-19 16:41:25 +0530

    day 80

--- DynamicProgramming/a.cpp
+++ DynamicProgramming/a.cpp
@@ -1,41 +1,52 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int ans = 0;
+int dfs(vector<bool> vis, int k, int n)
+{
+    
+    int i 
+    
+        
+}
+
 int main()
 {
 #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 #endif
-    
-    int i, j, n;
-    
-    cin>>n;
-    int a[n], b[n];
-    
-    for(i=0; i<n; i++)
+
+    int i, j, k, n, t;
+
+    cin >> t;
+
+    while (t--)
     {
-        cin>>a[i];
-        b[i] = a[i];
+        cin >> n;
+        vector<bool> vis(n, false);
+        int ans2 = 0;
+
+        for (i = 0; i < n; i++)
+        {
+            
+            vis[i] = true;
+            k = dfs(vis, i, n);
+            
+            for(j = 0; j<n; j++)
+            {
+                if(!vis[j])
+                    break;
+            }
+            
+            if(j<n)
+                ans2 = min(ans2, k);
+            
+            
+        }
+
+        cout << ans2 << endl;
     }
-    
-    sort(a, a+n);
-    
-    int ans = 0;
-    for(i=0; i<n; i++)
-    {
-        if(a[i] != b[i])
-            ans++;
-    }
-    
-    if(ans > 2)
-    {
-        cout<<"NO";
-    }
-    else
-    {
-        cout<<"YES";
-    }
-    
+
     return 0;
 }
