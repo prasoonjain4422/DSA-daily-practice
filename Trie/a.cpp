75e923e80a4921c27dfb42a9ca1af3e0af44377d
Prasoon Jain <prasoonjain4422@gmail.com>
2021-09-22 15:16:18 +0530

    day 83

--- Trie/a.cpp
+++ Trie/a.cpp
@@ -1,153 +1,79 @@
-// { Driver Code Starts
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Node
+string ans;
+class Solution
 {
-    int data;
-    struct Node *left;
-    struct Node *right;
+    public:
+    
+    static void dfs(string str, int k, int i)
+    {
+        
+        if((k <= 0)||(i >= str.size()))
+            return;
+        
+
+        int j, maxn;
+        maxn = str[i]-48;
+        for(j=i+1; j<str.size(); j++)
+        {
+            if((str[j]-48) > maxn)
+            {
+                maxn = str[j]-48;
+            }
+        }
+
+
+        if(maxn != (str[i]-48))
+        {
+            
+            for(int j = i+1; j<str.size(); j++)
+            {
+                if(maxn == (str[j]-48))
+                {
+                    swap(str[i], str[j]);
+                    if(str > ans)
+                    {
+                        ans = str;
+                    }
+                    dfs(str, k-1, i+1);
+                    swap(str[i], str[j]);
+                }
+            }
+        }
+        else
+        {
+            dfs(str, k, i+1);
+        }
+        
+    }
+    
+    
+
+    string findMaximumNum(string str, int k)
+    {
+        
+        ans = str;
+        dfs(str, k, 0);
+        
+        return ans;
+        
+    }
 };
-// Utility function to create a new Tree Node
-Node *newNode(int val)
-{
-    Node *temp = new Node;
-    temp->data = val;
-    temp->left = NULL;
-    temp->right = NULL;
 
-    return temp;
-}
-// Function to Build Tree
-Node *buildTree(string str)
-{
-    // Corner Case
-    if (str.length() == 0 || str[0] == 'N')
-        return NULL;
-
-    // Creating vector of strings from input
-    // string after spliting by space
-    vector<string> ip;
-
-    istringstream iss(str);
-    for (string str; iss >> str;)
-        ip.push_back(str);
-
-    // Create the root of the tree
-    Node *root = newNode(stoi(ip[0]));
-
-    // Push the root to the queue
-    queue<Node *> queue;
-    queue.push(root);
-
-    // Starting from the second element
-    int i = 1;
-    while (!queue.empty() && i < ip.size())
-    {
-
-        // Get and remove the front of the queue
-        Node *currNode = queue.front();
-        queue.pop();
-
-        // Get the current node's value from the string
-        string currVal = ip[i];
-
-        // If the left child is not null
-        if (currVal != "N")
-        {
-
-            // Create the left child for the current node
-            currNode->left = newNode(stoi(currVal));
-
-            // Push it to the queue
-            queue.push(currNode->left);
-        }
-
-        // For the right child
-        i++;
-        if (i >= ip.size())
-            break;
-        currVal = ip[i];
-
-        // If the right child is not null
-        if (currVal != "N")
-        {
-
-            // Create the right child for the current node
-            currNode->right = newNode(stoi(currVal));
-
-            // Push it to the queue
-            queue.push(currNode->right);
-        }
-        i++;
-    }
-
-    return root;
-}
-
-int maxSum(Node *root)
-{
-    if(root == NULL)
-        return 0;
-    
-    int a = 0;
-    if(root->left != NULL)
-    {
-        if(root->left->left != NULL)
-            a += maxSum(root->left->left);
-        
-        if(root->left->right != NULL)
-            a += maxSum(root->left->right);
-    }
-    
-    if(root->right != NULL)
-    {
-        if(root->right->left != NULL)
-            a += maxSum(root->left->left);
-        
-        if(root->right->right != NULL)
-            a += maxSum(root->left->right);
-    }
-    
-    return max((root->data + a), (maxSum(root->left) + maxSum(root->right)));
-}
 
 int main()
 {
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
+    int t, k;
+    string str;
 
-    // ios_base::sync_with_stdio(false);
-    // cin.tie(0);
-
-    int t;
-    scanf("%d ", &t);
+    cin >> t;
     while (t--)
     {
-        string s;
-        getline(cin, s);
-        Node *root = buildTree(s);
-        cout << maxSum(root) << endl;
+        cin >> k >> str;
+        Solution ob;
+        cout<< ob.findMaximumNum(str, k) << endl;
     }
     return 0;
-} // } Driver Code Ends
-
-/* A binary tree node structure
-
-struct Node
-{
-    int data;
-    struct Node* left;
-    struct Node* right;
-    
-    Node(int x){
-        data = x;
-        left = right = NULL;
-    }
-};
- */
-
-//Function to check whether a binary tree is balanced or not.
-
+}
+  // } Driver Code Ends
