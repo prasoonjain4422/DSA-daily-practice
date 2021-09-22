bbfcbd967d73fb574e9c7d4e29cb90cb5672af6a
Prasoon Jain <prasoonjain4422@gmail.com>
2021-09-22 16:31:39 +0530

    day 84

--- Trie/construct-trie-from-scratch.cpp
+++ Trie/construct-trie-from-scratch.cpp
@@ -1,6 +1,68 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+struct TrieNode
+{
+    TrieNode *child[26];
+    bool isEnd = false;
+};
+
+class Trie
+{
+    private:
+        struct TrieNode* root = new TrieNode();
+
+    public:
+        TrieNode *newNode()
+        {
+            TrieNode *temp = new TrieNode();
+            return temp;
+        }
+
+        void insert(string s)
+        {
+            int i, n = s.length();
+            TrieNode* itr = root;
+
+            for (i = 0; i < n; i++)
+            {
+                if (itr->child[s[i] - 'a'] == NULL)
+                {
+                    itr->child[s[i] - 'a'] = newNode();
+                    itr = itr->child[s[i] - 'a'];
+                }
+                else
+                {
+                    itr = itr->child[s[i] - 'a'];
+                }
+            }
+            itr->isEnd = true;
+        }
+
+        bool search(string s)
+        {
+            int i, n = s.length();
+            TrieNode* itr = root;
+
+            for (i = 0; i < n; i++)
+            {
+                if (itr->child[s[i] - 'a'] == NULL)
+                {
+                    return false;
+                }
+                else
+                {
+                    itr = itr->child[s[i] - 'a'];
+                }
+            }
+            
+            if(itr->isEnd)
+                return true;
+            
+            return false;
+        }
+};
+
 int main()
 {
 
@@ -10,12 +72,27 @@ int main()
 #endif
     
     
+    Trie obj;
+    
+    obj.insert("nature");
+    obj.insert("tendency");
+    obj.insert("characterstics");
     
     
+    if(obj.search("nature"))
+        cout<<"Found"<<endl;
+    else
+        cout<<"Not Found"<<endl;
     
+    if(obj.search("natur"))
+        cout<<"Found"<<endl;
+    else
+        cout<<"Not Found"<<endl;
     
-    
-    
+    if(obj.search("nature"))
+        cout<<"Found"<<endl;
+    else
+        cout<<"Not Found"<<endl;
     
     
     
