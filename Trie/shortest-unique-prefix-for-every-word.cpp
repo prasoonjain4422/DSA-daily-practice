72b0472bc26fda505d120095aba50525ea042ade
Prasoon Jain <prasoonjain4422@gmail.com>
2021-09-23 17:21:12 +0530

    day 85

--- Trie/shortest-unique-prefix-for-every-word.cpp
+++ Trie/shortest-unique-prefix-for-every-word.cpp
@@ -1,40 +1,146 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-long dfs(int n, int g, int i)
+struct TrieNode
 {
+    TrieNode *child[26];
+    int freq = 1;
+    bool isEnd = false;
+};
 
-    if ((g == 0) && (n == 0))
-        return 1;
+class Trie
+{
+private:
+    struct TrieNode *root = new TrieNode();
 
-    if (g <= 0)
-        return 0;
-
-    if (n <= 0)
-        return 0;
-
-    // cout << n << " " << g << endl;
-    long ans = 0;
-    for (i; i <= (n - g + 1); i++)
+public:
+    TrieNode *newNode()
     {
-        ans += dfs(n - i, g - 1, i);
-        // cout << n << " " << g << " ";
-        // cout << "i" << i << " -->" << ans << endl;
+        TrieNode *temp = new TrieNode();
+        temp->freq = 1;
+        return temp;
     }
 
-    // cout << n << " " << g << " " << ans << "  anssssssssss" << endl;
-    return ans;
-}
+    void insert(string s)
+    {
+        int i, n = s.length();
+        TrieNode *itr = root;
+
+        for (i = 0; i < n; i++)
+        {
+            if (itr->child[s[i] - 'a'] == NULL)
+            {
+                itr->child[s[i] - 'a'] = newNode();
+                itr = itr->child[s[i] - 'a'];
+                cout << " - " << s[i] << " " << itr->freq << endl;
+            }
+            else
+            {
+                itr = itr->child[s[i] - 'a'];
+                itr->freq++;
+                cout << " - " << s[i] << " " << itr->freq << endl;
+            }
+        }
+        itr->isEnd = true;
+    }
+
+    bool search(string s)
+    {
+        int i, n = s.length();
+        TrieNode *itr = root;
+
+        for (i = 0; i < n; i++)
+        {
+            if (itr->child[s[i] - 'a'] == NULL)
+            {
+                return false;
+            }
+            else
+            {
+                itr = itr->child[s[i] - 'a'];
+            }
+        }
+
+        if (itr->isEnd)
+            return true;
+
+        return false;
+    }
+
+    void dfs(TrieNode *itr, string cur, vector<string> &ans)
+    {
+        if (itr == NULL)
+            return;
+
+        if (itr->freq == 1)
+        {
+
+            // cur += " -> " + cur;
+
+            // for (int i = 0; i < 26; i++)
+            // {
+            //     if (itr->child[i] != NULL)
+            //     {
+            //         cur +=
+            //     }
+            // }
+
+            ans.push_back(cur);
+            return;
+        }
+
+        for (int i = 0; i < 26; i++)
+        {
+            if (itr->child[i] != NULL)
+            {
+                char ch = 'a' + i;
+                cout << cur << endl;
+                cur.append(1, ch);
+                cout << cur << endl;
+                dfs(itr, cur, ans);
+            }
+        }
+    }
+
+    void prefix(vector<string> &ans)
+    {
+
+        TrieNode *itr = root;
+
+        for (int i = 0; i < 26; i++)
+        {
+            if (itr->child[i] != NULL)
+            {
+                string s = string(1, 'a' + i);
+                dfs(itr, s, ans);
+            }
+        }
+    }
+};
 
 int main()
 {
+
 #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 #endif
 
-    int n, g;
-    cin >> n >> g;
-    cout << dfs(n, g, 1);
+    Trie obj;
+
+    obj.insert("nature");
+    obj.insert("natural");
+    obj.insert("tendency");
+    obj.insert("characterstics");
+
+    vector<string> ans;
+
+    obj.prefix(ans);
+
+    for (auto x : ans)
+    {
+        cout << x << endl;
+    }
+
     return 0;
 }
