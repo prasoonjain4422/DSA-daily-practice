650f1990ca930b4ff50cf70b3a3677a9b9458557
Prasoon Jain <prasoonjain4422@gmail.com>
2021-10-22 18:13:18 +0530

    day 93

--- DynamicProgramming/code_here.cpp
+++ DynamicProgramming/code_here.cpp
@@ -4,19 +4,48 @@
 using namespace std;
 const ll M = 1000000007;
 
+void dfs(ll i, vector<bool> &in, vector<ll> adj[])
+{
+
+    in[i] = true;
+
+    for (auto x : adj[i])
+    {
+        if (!in[x])
+        {
+            dfs(x, in, adj);
+        }
+    }
+}
 
 void solve()
 {
-    ll i, j, k, n, b, m, c, l, r, mid, sum;
-    
-    cin >> n >> k;
-    
-    
-    
-    
-    
-    
-    
+    ll i, j, k, n, a, b, m, c, l, r, mid, sum, f;
+
+    // cin >> n >> m;
+    priority_queue<pair<ll, ll>, vector<pair<ll, ll>>, greater<pair<ll, ll>>> pq;
+
+    pq.push({5, 1});
+    pq.push({2, 1});
+    pq.push({4, 1});
+    pq.push({1, 3});
+    pq.push({3, 2});
+    n = 1;
+
+    while (!pq.empty())
+    {
+        cout << pq.top().first << " " << pq.top().second << endl;
+        pq.pop();
+
+        if (n == 1)
+        {
+            pq.push({1, 3});
+            pq.push({2, 2});
+            n = 0;
+        }
+    }
+
+    // cout << endl;
 }
 
 int main()
